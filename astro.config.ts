4d12b779baba062e1aa1ac5ce838f08f25673e0a
diamondburned <diamond@arikawa-hi.me>
2023-04-11 13:08:51 -0700

    Modify configs however needed

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "astro/config";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { config as viteConfig } from "./vite.config.js";
+import path from "path";
 
 export default defineConfig({
   // your configuration options here...
   // https://docs.astro.build/en/reference/configuration-reference/
   output: "server",
+  server: {
+    host: true,
+    port: 3000,
+  },
+  vite: viteConfig,
 });
--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,15 @@ import { defineConfig } from "vite";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import path from "path";
 
-// https://github.com/vitejs/vite/issues/7385#issuecomment-1286606298
-export default defineConfig({
+export const config = {
   resolve: {
     alias: {
-      "#lib": path.resolve(__dirname, "./src/lib/"),
-      "#pages": path.resolve(__dirname, "./src/pages/"),
-      "#components": path.resolve(__dirname, "./src/components/"),
+      "#lib": path.resolve("./src/lib/"),
+      "#pages": path.resolve("./src/pages/"),
+      "#components": path.resolve("./src/components/"),
     },
   },
-});
+};
+
+// https://github.com/vitejs/vite/issues/7385#issuecomment-1286606298
+export default defineConfig(config);
